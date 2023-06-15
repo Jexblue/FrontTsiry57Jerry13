9ff19152350519aed3287f69719961195e04080e
Tsiry <razafindrakoto0tsiry@gmail.com>
2023-06-15 15:51:13 +0300

    update conf

---
+++ src/app/shared/matiere_data.ts
@@ -0,0 +1,6 @@
+export const bdInitialEtudiants = [
+    { "nom": "Base de donnees", "pathPhoto": null, "pathProf": null },
+    { "nom": "Java avance", "pathPhoto": null, "pathProf": null },
+    { "nom": "Angular", "pathPhoto": null, "pathProf": null },
+    { "nom": "Grails", "pathPhoto": null, "pathProf": null },
+]
